7b22f6d69becb03762d8a0b3289e9b96ac4bea49
PhamTruong <truong.pham@hdwebsoft.co>
2018-04-09 17:25:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pctruong/customtablayout/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.pctruong.customtablayout;
+
+import android.app.Fragment;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private CustomViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_tab_favourite,
+            R.drawable.ic_tab_call,
+            R.drawable.ic_tab_contacts,
+            R.drawable.ic_tab_contacts
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager=findViewById(R.id.viewpager);
+        tabLayout=findViewById(R.id.tabLayout);
+        viewPager.setPagingEnabled(false);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+    }
+    private void setupTabIcons() {
+
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText("ONE");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_tab_favourite, 0, 0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabTwo.setText("TWO");
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_tab_call, 0, 0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabThree.setText("THREE");
+        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_tab_contacts, 0, 0);
+        tabLayout.getTabAt(2).setCustomView(tabThree);
+
+        TextView tabFour = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabFour.setText("FOOUR");
+        tabFour.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_tab_contacts, 0, 0);
+        tabLayout.getTabAt(3).setCustomView(tabFour);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new FragmentOne(), "ONE");
+        adapter.addFrag(new Fragment_Two(), "TWO");
+        adapter.addFrag(new Fragment_Three(), "THREE");
+        adapter.addFrag(new Fragment_Three(), "FOOUR");
+        viewPager.setAdapter(adapter);
+    }
+}
